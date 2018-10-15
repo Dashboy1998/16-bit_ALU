82231aaf3238cad255a6a24464cd3c827eb6996e
Dashboy1998 <dashboy1998@gmail.com>
2018-10-15 10:40:36 -0400

    Added 4bit AND gate, finished mulitplexer

--- VHDL Workspace 6/src/Mux_8-1.vhd
+++ VHDL Workspace 6/src/Mux_8-1.vhd
@@ -1,16 +1,46 @@
 library ieee;
 library gate_lib;
 use gate_lib.all;
+use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
 entity Mux_8 is
 	port(
-	I: in std_logic_vector(7 downto 0):=(others => '0'); -- Inputs of mux
-	S: in std_logic_vector(2 downto 0):=(others => '0');	-- Select Lines
-	Z: out std_logic:='0' -- Output data
-	);
+		I0, I1, I2, I3, I4, I5, I6, I7: in std_logic:='0'; -- Inputs of mux
+		S: in std_logic_vector(2 downto 0):=(others => '0');	-- Select Lines
+		Z: out std_logic:='0' -- Output data
+		);
 end Mux_8;
 
 architecture structural of Mux_8 is
+	component and4
+		generic(pDelay : time := 0 ns);
+		port(A, B, C, D: in std_logic; F: out std_logic);	
+	end component and4;
+	component or_gate  
+		generic(pDelay : time := 0 ns);
+		port(A: in std_logic_vector; C : out std_logic);	
+	end component or_gate;
+	component not1
+		generic(pDelay : time := 0 ns);
+		port(A: in std_logic; B: out std_logic);
+	end component not1;
+	signal ANDn: std_logic_vector(0 to 7);
+	signal nS : std_logic_vector(2 downto 0);
 begin
+	notS: for i in 0 to 2 generate -- Creates nots of the select lines
+		begin
+		Sn: not1 port map(S(i), nS(i));
+	end generate;
+	-- AND gates of the MUX
+	AND_0: and4 port map (I0, nS(2), nS(1), nS(0), ANDn(0));
+	AND_1: and4 port map (I1, nS(2), nS(1), S(0), ANDn(1));
+	AND_2: and4 port map (I2, nS(2), S(1), nS(0), ANDn(2));
+	AND_3: and4 port map (I3, nS(2), S(1), S(0), ANDn(3));
+	AND_4: and4 port map (I4, S(2), nS(1), nS(0), ANDn(4));
+	AND_5: and4 port map (I5, S(2), nS(1), S(0), ANDn(5));
+	AND_6: and4 port map (I6, S(2), S(1), nS(0), ANDn(6));
+	AND_7: and4 port map (I7, S(2), S(1), S(0), ANDn(7)); 
+	
+	O: or_gate port map(ANDn, Z); -- OR gate of the MUX
 end structural;
---
+++ gate_lib/src/and4.vhd
@@ -0,0 +1,15 @@
+-- 4 input AND gate
+library ieee;
+use ieee.std_logic_1164.all;
+use work.gate_delay.all; -- Used to get default propagation delay
+
+
+entity and4 is
+	generic(pDelay: time := propagation);
+	port(A, B, C, D: in std_logic:='0'; F: out std_logic:='0');
+end entity and4;
+
+architecture and4 of and4 is
+begin
+	F <= A and B and C and D after propagation;
+end architecture;
